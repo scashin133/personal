9cb7446bb8a3e6cf2bcb2e2354e4161afdeb120c
Sean <scashin133@gmail.com>
2008-10-14 22:24:20 -0700

    Added in Sass stylesheet.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -3,7 +3,7 @@
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-
+  layout 'application'
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => 'c9d2482012ee8835a1e55825c030b4b7'
