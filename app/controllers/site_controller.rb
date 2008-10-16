94f87790091e95f4dd054234e5158f7f65af7c15
Sean <scashin133@gmail.com>
2008-10-15 21:11:56 -0700

    Making weather form sweet ajaxy goodness

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,20 +3,24 @@ class SiteController < ApplicationController
   include GeoKit::Geocoders
 
   def index    
-    service = Weather::Service.new
-    
-    service.partner_id = "1077495283"
-    service.license_key = "193871af075cd39b"
-    service.imperial = true
-    if params[:address]
+  end
+  
+  def find_address
+
+      service = Weather::Service.new
+
+      service.partner_id = "1077495283"
+      service.license_key = "193871af075cd39b"
+      service.imperial = true
+      
       @address = params[:address]
 
       @weather_locations = service.find_location(@address)
-      
+
       @forecasts = service.fetch_forecast(@weather_locations.keys[0])
-    end
 
-
+      render :partial => 'site/find_address',:layout => false
+        
   end
 
 end
