3319edb944515abdba824e10a86a8d7a9ce0bacf
Sean <scashin133@gmail.com>
2008-12-03 19:38:48 -0800

    Starting over

--- vendor/plugins/svn/init.rb
+++
@@ -1,3 +0,0 @@
-require File.dirname(File.expand_path(__FILE__)) + '/lib/weather/util'
-require File.dirname(File.expand_path(__FILE__)) + '/lib/weather/service'
-require File.dirname(File.expand_path(__FILE__)) + '/lib/weather/forecast'
--- vendor/plugins/svn/weather.rb
+++
@@ -1,5 +0,0 @@
-# DEPRECATED
-# This file will likely go away within the next few versions.
-# Use init.rb instead, or just require 'weather/service'
-
-require File.dirname(File.expand_path(__FILE__)) + '/init'
